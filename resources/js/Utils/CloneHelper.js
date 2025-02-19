84be74bbb876984cad2bd7c4d72c6869039bb2f1
dev@steiiin.de <dev@steiiin.de>
2025-02-19 11:17:23 +0100

    fix: clone empty strings correctly

--- resources/js/Utils/CloneHelper.js
+++ resources/js/Utils/CloneHelper.js
@@ -2,7 +2,7 @@ export const CloneHelper = {
 
   payload(obj) { return JSON.parse(JSON.stringify(obj)) },
   date(date) { return new Date(date) },
-  string(str) { return (' ' + str).slice(1) },
+  string(str) { return !str ? '' : (' ' + str).slice(1) },
   obj(obj) { return { ...obj }},
 
 };
