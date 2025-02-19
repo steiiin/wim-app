7b88410c9ac6e5b2977881522922b816d58a7b04
dev@steiiin.de <dev@steiiin.de>
2025-02-19 08:36:31 +0100

    refactor: remove unused use

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Modules\TrashController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/trash', [ TrashController::class, 'update' ]);
