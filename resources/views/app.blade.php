df92bdc2afa9d4993da9e2478ecd50d968f155b0
dev@steiiin.de <dev@steiiin.de>
2025-03-28 07:39:12 +0100

    feat: add PWA support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" translate="no" class="{{ request()->routeIs('monitor') ? 'no-scrollbars' : '' }}">
     <head>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="google" content="notranslate">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- PWA support -->
+        <meta name="theme-color" content="#ffffff">
+        <link rel="icon" href="/favicon.ico" sizes="48x48">
+        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml">
+        <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png">
 
         <!-- Scripts -->
         @routes
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
@@ -16,11 +18,46 @@ export default defineConfig({
                 },
                 compilerOptions: {
                     isCustomElement: (tag) => [
-                        'content','hint',
-                        'name','clock','date','issues','info','error',
-                        'content-title','content-list','article-pre','article-payload','payload-title','vehicle','payload-description','payload-meta','payload-timing'].includes(tag),
+                        'content', 'hint',
+                        'name', 'clock', 'date', 'issues', 'info', 'error',
+                        'content-title', 'content-list', 'article-pre', 'article-payload', 'payload-title', 'vehicle', 'payload-description', 'payload-meta', 'payload-timing'].includes(tag),
                 },
             },
         }),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
+            manifest: {
+                name: 'WIM-Admin',
+                short_name: 'WIM-Admin',
+                description: 'App, um die Einträge im WIM anzupassen.',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any'
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ]
+            },
+            devOptions: { enabled: false }
+        })
     ],
 });
