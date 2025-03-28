eb318c7b9002a6724a7f6f1762d5d9c1532ff289
dev@steiiin.de <dev@steiiin.de>
2025-03-28 13:28:37 +0100

    fix: remove VitePWA

--- vite.config.js
+++ vite.config.js
@@ -24,40 +24,5 @@ export default defineConfig({
                 },
             },
         }),
-        VitePWA({
-            registerType: 'autoUpdate',
-            includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.png'],
-            manifest: {
-                name: 'WIM-Admin',
-                short_name: 'WIM-Admin',
-                description: 'App, um die Einträge im WIM anzupassen.',
-                theme_color: '#ffffff',
-                icons: [
-                    {
-                        src: 'pwa-64x64.png',
-                        sizes: '64x64',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'pwa-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'any'
-                    },
-                    {
-                        src: 'maskable-icon-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable'
-                    }
-                ]
-            },
-            devOptions: { enabled: false }
-        })
     ],
 });
